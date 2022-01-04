239af488bba6c5f24b73580aec09bca19a746f87
Bulat Faizrakhmanov <58148408+btfv@users.noreply.github.com>
2022-01-05 00:59:34 +0300

    quick fix

--- src/components/dashboard/HomeworksList.js
+++ src/components/dashboard/HomeworksList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import HomeworkCard from './HomeworkCard';
